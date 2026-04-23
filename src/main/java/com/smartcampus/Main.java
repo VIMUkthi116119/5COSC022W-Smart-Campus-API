fcf9a92bb672e0549e746fe76ee2a9e90094aeed
Vimukthi116119e <vimukthiubeysekera@gmail.com>
2026-04-23 12:57:45 +0530

    Setup: Created Main.java entry point with Grizzly server configuration

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,35 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class to start the Smart Campus API server.
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // scan packages for resources and providers
+        final ResourceConfig rc = new ResourceConfig().packages("com.smartcampus");
+
+        // create and start a new instance of grizzly http server
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started at %s\nHit ENTER to stop it...", BASE_URI));
+        System.in.read();
+        server.shutdownNow();
+    }
+}
